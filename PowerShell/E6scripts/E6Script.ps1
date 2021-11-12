9a24ec66b1d851186e5a0f32af13fab63c7ffa9f
Edwinsta <90713354+Edwinsta@users.noreply.github.com>
2021-11-12 11:58:23 -0600

    Add files via upload

---
+++ PowerShell/E6scripts/E6Script.ps1
@@ -0,0 +1,27 @@
+﻿# Edwin Soto Arregoitia, Adrian Alejandro Treviño Bautista, Sofia Flores Cisneros
+#Este script amigable te ayudara en caso de no saber la hora fecha, el calendario
+#O buscar doc. de una fecha especifica
+#Fuentes de consulta https://crisroblesti.wordpress.com/2018/02/13/funciones-en-powershell/
+import-module -name Seteolvidolahora 
+clear-host 
+function get-menu{
+$Menu= Read-Host "Elige una opcion: 
+1:¿Deseas ver el calendario?
+2:¿Desea ver la hora o la fecha?                                               
+3:¿Deseas ver archivos de una fecha en concreto?
+4:Salida                                                                         
+Opción"
+clear-host
+Switch($Menu){
+  1{Show-Calendar -ErrorAction SilentlyContinue} 
+  2{Hora-fecha}
+  #Si los datos no se colocan bien marcara error
+  3{Archivos-fecha}
+  4{Exit}
+  Default {Write-Host "Opción no valida" -ErrorAction SilentlyContinue}
+}} 
+
+do{
+  ""
+  get-menu
+  }while($true)
---
+++ PowerShell/E6scripts/Seteolvidolahora.psm1
@@ -0,0 +1,156 @@
+﻿function Show-Calendar { 
+
+    param( 
+
+        [DateTime] $start = [DateTime]::Today, 
+
+        [DateTime] $end = $start, 
+
+        $firstDayOfWeek, 
+
+        [int[]] $highlightDay, 
+
+        [string[]] $highlightDate = [DateTime]::Today.ToString() 
+
+    ) 
+
+    $start = New-Object DateTime $start.Year,$start.Month,1 
+
+    $end = New-Object DateTime $end.Year,$end.Month,1 
+
+    $dateTimeFormat  = (Get-Culture).DateTimeFormat 
+
+    if($firstDayOfWeek) 
+
+    { 
+
+        $dateTimeFormat.FirstDayOfWeek = $firstDayOfWeek 
+
+    } 
+
+ 
+
+    $currentDay = $start 
+    while($start -le $end) 
+
+    { 
+
+        while($currentDay.DayOfWeek -ne $dateTimeFormat.FirstDayOfWeek) 
+
+        { 
+
+            $currentDay = $currentDay.AddDays(-1) 
+
+        } 
+        $currentWeek = New-Object PsObject 
+
+        $dayNames = @() 
+
+        $weeks = @() 
+
+        while(($currentDay -lt $start.AddMonths(1)) -or 
+
+            ($currentDay.DayOfWeek -ne $dateTimeFormat.FirstDayOfWeek)) 
+
+        { 
+            $dayName = "{0:ddd}" -f $currentDay 
+
+            if($dayNames -notcontains $dayName) 
+
+            { 
+
+                $dayNames += $dayName 
+
+            } 
+            $displayDay = " {0,2} " -f $currentDay.Day 
+
+            if($highlightDate) 
+
+            { 
+
+                $compareDate = New-Object DateTime $currentDay.Year, 
+
+                    $currentDay.Month,$currentDay.Day 
+
+                if($highlightDate -contains $compareDate) 
+
+                { 
+
+                    $displayDay = "*" + ("{0,2}" -f $currentDay.Day) + "*" 
+
+                } 
+
+            } 
+            if($highlightDay -and ($highlightDay[0] -eq $currentDay.Day)) 
+
+            { 
+
+                $displayDay = "[" + ("{0,2}" -f $currentDay.Day) + "]" 
+
+                $null,$highlightDay = $highlightDay 
+
+            } 
+            $currentWeek | Add-Member NoteProperty $dayName $displayDay 
+            $currentDay = $currentDay.AddDays(1) 
+            if($currentDay.DayOfWeek -eq $dateTimeFormat.FirstDayOfWeek) 
+
+            { 
+
+                $weeks += $currentWeek 
+
+                $currentWeek = New-Object PsObject 
+
+            } 
+
+        } 
+
+        $calendar = $weeks | Format-Table $dayNames -AutoSize | Out-String 
+
+        $width = ($calendar.Split("`n") | Measure-Object -Maximum Length).Maximum 
+
+        $header = "{0:MMMM yyyy}" -f $start 
+
+        $padding = " " * (($width - $header.Length) / 2) 
+
+        $displayCalendar = " `n" + $padding + $header + "`n " + $calendar 
+
+        $displayCalendar.TrimEnd() 
+
+        $start = $start.AddMonths(1) 
+
+ 
+
+    } 
+
+} -ErrorAction SilentlyContinue
+
+function Hora-fecha{
+$Menu= Read-Host "Elige una opcion: 
+1:¿Deseas ver la hora?
+2:¿O desea ver la hora y la fecha?                                                                                                                       
+Opción"
+clear-host
+Switch($Menu){
+  1{Get-Date -DisplayHint Time} 
+  2{Get-Date}
+  Default {Write-Host "Opción no valida" -ErrorAction SilentlyContinue}
+}}
+
+Function Archivos-fecha{
+param (
+            [string]$Ruta = "",
+            [string]$Type = "",
+            [int32]$month = "",
+            [int32]$year = ""
+)
+    $Ruta = Read-Host "Dame la ruta: Ejem D:"
+    $Type = Read-Host "Dame el tipo: Ejem docx"
+    $month = Read-Host "Dame el mes: Ejem 02"
+    $year = Read-host "Dame el año: Ejem 2020"
+
+
+Get-ChildItem -Path $Ruta -Include *.$Type -Recurse -File | 
+    Where-Object { $_.lastwritetime.month -eq $month -AND $_.lastwritetime.year -eq $year}  
+}
+
+Export-ModuleMember -Function Show-Calendar -Function Hora-fecha -Function Archivos-fecha
